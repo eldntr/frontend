139a76f10d32bf40e793fa1c5d5a161009199b0e
Irham Maulana <76547387+kitokato77@users.noreply.github.com>
2024-10-13 13:57:17 +0000

    Update Order.php
    add orderitems function to help other variables

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -21,4 +21,9 @@ class Order extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
